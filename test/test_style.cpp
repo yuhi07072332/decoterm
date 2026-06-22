aee73d9d984a6bc27ecaaeabdf9c7f0748100625
Yuhi0707 <yuhi07072332@gmail.com>
2026-06-22 16:14:54 +0900

    add test_style

--- test/test_style.cpp
+++ test/test_style.cpp
@@ -1,7 +1,32 @@
-#include "../include/decoterm/decoterm.hpp"
+#include <decoterm/decoterm.hpp>
 
-#include <iostream>
+#include <array>
+#include <print>
+#include <string_view>
+
+using namespace deco;
+
+constexpr std::array<std::string_view, 8> style_names {
+    "Bold",
+    "Dim",
+    "Italic",
+    "Underline",
+    "Blink",
+    "Invert",
+    "Strikethrough",
+    "UnderlineDouble",
+};
 
 int main() {
-    std::cout << (deco::italic | deco::fg(deco::yellow)) << "Hello world!\n";
+    std::println("Normal");
+    for (int i = 0; i < style_names.size(); ++i) {
+        std::print(
+            "{}{}{}  ",
+            Style(static_cast<Style::StyleFlags>(1 << i)),
+            style_names[i],
+            reset
+        );
+
+        std::println();
+    }
 }
