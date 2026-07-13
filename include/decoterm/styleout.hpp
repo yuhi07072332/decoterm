990ac67fdfea018fb82c71117b203a0a9d67cc72
Yuhi0707 <yuhi07072332@gmail.com>
2026-07-13 09:58:51 +0900

    wip: styleout

--- include/decoterm/styleout.hpp
+++ include/decoterm/styleout.hpp
@@ -11,13 +11,52 @@
 #include "style.hpp"
 
 #include <ostream>
+#include <vector>
+#include <variant>
 
 namespace deco {
 
+namespace detail {
+
+}
+
+/// @brief A class representing Style output state.
+class StyleOutState {
+public:
+
+    StyleOutState() = default;
+
+    // ----- options -----
+
+    void set_style_output(bool enable) { style_output_enabled_ = enable; }
+
+    void set_style_stack(bool enable) { stack_enabled_ = enable; }
+
+    void set_color_fallback(bool enable) { color_fallback_enabled_ = enable; }
+
+    // ----- observe -----
+
+    auto current_style() const -> AbsoluteStyle {
+
+    }
+
+private:
+    std::variant<AbsoluteStyle, std::vector<AbsoluteStyle>> stack_;
+    bool style_output_enabled_ = true;
+    bool stack_enabled_ = true;
+    bool color_fallback_enabled_ = false;
+};
+
 /// @brief A lightweight stateful writer over an existing std::ostream.
-class StyleOut {
+class StyleOut : StyleOutState{
 public:
     StyleOut(std::ostream& os) : os_(os) {}
+
+    template <typename T>
+    friend auto operator<< (StyleOut& sout, T&& value) -> StyleOut& {
+        sout.os_ << value;
+        return sout;
+    }
 
 private:
     std::ostream& os_;
