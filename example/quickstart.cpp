f4b2c2b2ec12bddf1c58fe65223e534993c24ecd
Yuhi0707 <yuhi07072332@gmail.com>
2026-07-24 09:11:40 +0900

    quickstart

--- example/quickstart.cpp
+++ example/quickstart.cpp
@@ -3,18 +3,20 @@
 #include <iostream>
 #include <string_view>
 
+// clang-format off
+
 auto main() -> int {
     // -- 1. Basic Style output
 
     deco::Style style = deco::underline | deco::fg(deco::bright_green);
-    deco::Style warning_style = deco::bold | deco::fg(deco::rgb(0xfbfcc5));
+    deco::Style warning_style = deco::bold | deco::fg(deco::rgb(0xfff2b2));
 
     std::cout   << (deco::bold | deco::fg(deco::bright_blue))
                 << "Hello "
                 << style
                 << "World!"
                 << deco::reset      // Reset to default style
-                << '\n';
+                << "\n\n";
 
     // just like fmt::styled() from fmtlib
     std::cout << deco::styled("warning: ", warning_style)
@@ -22,7 +24,7 @@ auto main() -> int {
               << deco::styled("[-Wunused-variable]", warning_style | deco::italic)
               << '\n';
 
-    // HSV usage (idk)
+    // HSV usage (maybe)
     std::string_view text = "Rainbooowww~~~~~~~";
     std::cout << deco::italic;
     for (std::size_t i = 0; i < text.size(); ++i) {
@@ -31,11 +33,20 @@ auto main() -> int {
     }
     std::cout << "\n\n";
 
-    // -- 2. Advanced
+    // -- 2. Advanced stateful output
 
-    deco::StyledOstream styled_out = deco::styled_out(std::cout)
-        .base_style(deco::bg(deco::rgb(64, 64, 64)))
-        .enable_style(deco::terminal::is_stdout_tty())
+    deco::StyledOstream sout = deco::styled_out(std::cout)
+        .base_style(deco::bg(deco::rgb(32, 32, 32)))
         .enable_nesting();
 
+    // Style nesting
+    sout    << "base style "
+                << deco::fg(deco::bright_blue) << "{ blue "
+                    << deco::italic << "{ italic blue "
+                        << deco::fg(deco::bright_red) << "{ italic red } "
+                    << deco::pop << "italic blue } "
+                << deco::pop << "blue } "
+            << deco::pop << "base style";
+
+    
 }
