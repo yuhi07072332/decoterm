d7bc068e7a3847390c87ff1982661eac45d15822
Yuhi0707 <yuhi07072332@gmail.com>
2026-08-04 11:46:31 +0900

    benchmark

--- benchmark/benchmark.hpp
+++ benchmark/benchmark.hpp
@@ -2,40 +2,61 @@
 #define DECOTERM_BENCHMARK_HPP
 
 #include <decoterm/style.hpp>
+#include <decoterm/format.hpp>
 
+#include <cstdint>
 #include <chrono>
 #include <concepts>
 #include <string_view>
 
-inline auto measure(auto&& fn) -> double {
+template <typename Fn>
+inline auto measure(Fn&& fn, uint64_t i = 0) -> double {    // NOLINT
     using namespace std::chrono_literals;
     auto begin = std::chrono::steady_clock::now();
-    fn();
+    if constexpr (std::invocable<Fn>) fn();
+    else if constexpr (std::invocable<Fn, uint64_t>) fn(i);
     auto end = std::chrono::steady_clock::now();
     return (end - begin) / 1ns / 1000000.0;     //NOLINT
 }
 
-inline auto measure_avg(auto&& fn, long long times) -> double {
+inline auto measure_avg(auto&& fn, uint64_t times) -> double {
     double avg = 0;
-    for (long long i = 0; i < times; ++i) {
-        avg += measure(fn) / times;
+    for (uint64_t i = 0; i < times; ++i) {
+        avg += measure(fn, i) / times;
     }
     return avg;
 }
 
-template <std::invocable<void> Fn>
+template <typename Fn>
+    requires std::invocable<Fn> || std::invocable<Fn, uint64_t>
 struct Entry {
     constexpr Entry(Fn fn)
         : fn(std::move(fn)) {}
 
-    constexpr auto times(int times) -> Entry& { times_ = times; return *this; }
+    constexpr auto name(std::string name) -> Entry& {
+        name_ = std::move(name);
+        return *this;
+    }
 
     Fn fn;
-    int times_ = 1;
+    std::string name_;
 };
 
+template <typename Fn>
+inline void run_entry(uint64_t times, Entry<Fn>& entry) {
+    double res = times == 1 ? measure(entry.fn) : measure_avg(entry.fn, times);
+    std::print("{:24}:   n={:<8}   {:.6}ms\n", entry.name_, times, res);
+}
+
+
 template <typename... Entries>
-inline void bench_mark(std::string_view name, Entries&&...entries) {
+inline void bench_mark(std::string_view name, uint64_t times = 1, Entries&&...entries /*NOLINT*/) {
+    auto sfmt = deco::styled_fmt();
+    sfmt.print(deco::bold | deco::fg(deco::bright_yellow), "BENCHMARK: {}", name)
+        .print("\n\n");
+
+    (run_entry(times, entries), ...);
+    sfmt.print("\n");
 }
 
 #endif // !DECOTERM_BENCHMARK_HPPt
--- benchmark/style_output.cpp
+++ benchmark/style_output.cpp
@@ -1,3 +1,67 @@
-
+#include <decoterm/format.hpp>
 #include <decoterm/style.hpp>
+
+#include <cstdint>
+#include <iostream>
+#include <print>
+#include <sstream>
+
 #include "benchmark.hpp"
+
+auto main(int argc, const char** argv) -> int {
+    using namespace deco;
+    const uint64_t times = argc == 2 ? std::stoi(argv[1]) : 1;
+
+    std::ostringstream os = std::ostringstream();
+
+    bench_mark(
+        "terminal color output",
+        times,
+        Entry([&] { os << "\x1b[34m\x1b[m"; }).name("ostream/raw escape"),
+        Entry([&] {
+            os << fg(blue) << reset;
+        }).name("ostream/Style output"),
+        Entry([&] { std::print(os, "\x1b[34m\x1b[m"); }).name("format/raw escape"),
+        Entry([&] {
+            std::print(os, "{}", fg(blue));
+        }).name("format/Style output"));
+
+    bench_mark("fixed RGB color output",
+               times,
+               Entry([&] {
+                   os << "\x1b[48;2;156;234;108m\x1b[;m";
+               }).name("ostream/raw escape"),
+               Entry([&] {
+                   os << bg(rgb(156, 234, 108));
+               }).name("ostream/Style output"),
+               Entry([&] {
+                   std::print(os, "\x1b[48;2;156;234;108m\x1b[;m");
+               }).name("format/raw escape"),
+               Entry([&] {
+                   std::print(os, "{}", bg(rgb(156, 234, 108)));
+               }).name("format/raw escape"));
+
+    bench_mark("dynamic RGB color output",
+               times,
+               Entry([&](uint64_t i) {
+                   os << "\x1b[48;2;" << (i & 255) << ';'
+                             << ((i >> 8) & 255) << ';' << ((i >> 16) & 255)
+                             << "m\x1b[;m";
+               }).name("ostream/raw escape"),
+               Entry([&](uint64_t i) {
+                   os
+                       << bg(rgb(i & 255, (i >> 8) & 255, (i >> 16) & 255))
+                       << reset;
+               }).name("ostream/Style output"),
+               Entry([&](uint64_t i) {
+                   std::print(os, "\x1b[48;2;{};{};{}m\x1b[;m",
+                              i & 255,
+                              (i >> 8) & 225,
+                              (i >> 16) & 255);
+               }).name("format/raw escape"),
+               Entry([&](uint64_t i) {
+                   std::print(os, "{}{}",
+                              bg(rgb(i & 255, (i >> 8) & 255, (i >> 16) & 255)),
+                              reset);
+               }).name("format/Style output"));
+}
